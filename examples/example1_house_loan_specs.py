fbae995ae1ef389448063eefa0bbca45b03739ab
uttambhat <uttambhat13@gmail.com>
2022-01-18 19:45:27 -0800

    First commit

--- examples/example1_house_loan_specs.py
+++ examples/example1_house_loan_specs.py
@@ -6,7 +6,7 @@ from personal_finance import *
 # loan specs, monthly payments etc.
 #====================================
 
-house_loan_1 = house_loan(house_price=1.2e6, down_payment=2.e5, loan_term_years=30, annual_interest_rate=3., property_tax_annual_rate=0.7)
+house_loan_1 = house_loan(house_price=350000., down_payment=50000., loan_term_years=20, annual_interest_rate=3., property_tax_annual_rate=0.7)
 house_loan_1.loan_specs(return_schedule=True)
 
 
--- examples/example2_to_buy_or_not_to_buy.py
+++ examples/example2_to_buy_or_not_to_buy.py
@@ -6,11 +6,11 @@ from personal_finance import *
 # Compare alternate investment scenarios - 1) rent and invest in market vs. 2) invest in a house
 #================================================================================================
 
-networth = alternate_investment_scenario(monthly_rent=3000., rent_inflation_rate=2., market_investment_return_rate=7., \
-                              house_loan_x=house_loan(house_price=1200000., down_payment=200000., loan_term_years=30, annual_interest_rate=3., property_tax_annual_rate=0.7), house_price_inflation_rate=2.)
+networth = alternate_investment_scenario(monthly_rent=1500., rent_inflation_rate=2., market_investment_return_rate=7., \
+                              house_loan_x=house_loan(house_price=500000., down_payment=50000., loan_term_years=20, annual_interest_rate=3., property_tax_annual_rate=0.7), house_price_inflation_rate=2.)
 
-print("Net worth if renting after 30 years: ",networth['renter_scenario_wealth'])
-print("Net worth if a home owner after 30 years: ",networth['home_owner_scenario_wealth'])
+print("Net worth if renting after 20 years: ",networth['renter_scenario_wealth'])
+print("Net worth if a home owner after 20 years: ",networth['home_owner_scenario_wealth'])
 
 
 
